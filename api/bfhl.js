91a1cded55c4bfafaa493b2ceac12ca753eece18
Kshitij Kumar <mail.kshitijshekhar@gmail.com>
2025-08-29 11:10:45 +0530

    Fix bfhl.js route for Vercel

--- api/bfhl.js
+++ api/bfhl.js
@@ -1,52 +1,48 @@
 module.exports = (req, res) => {
   if (req.method === "POST") {
-    try {
-      const data = req.body.data || [];
+    const data = req.body?.data || [];
 
-      let odd_numbers = [];
-      let even_numbers = [];
-      let alphabets = [];
-      let special_characters = [];
-      let sum = 0;
-      let allAlphabets = [];
+    let odd_numbers = [];
+    let even_numbers = [];
+    let alphabets = [];
+    let special_characters = [];
+    let sum = 0;
+    let allAlphabets = [];
 
-      data.forEach(item => {
-        if (/^\d+$/.test(item)) {
-          let num = parseInt(item);
-          sum += num;
-          if (num % 2 === 0) {
-            even_numbers.push(item);
-          } else {
-            odd_numbers.push(item);
-          }
-        } else if (/^[a-zA-Z]+$/.test(item)) {
-          alphabets.push(item.toUpperCase());
-          allAlphabets.push(item);
+    data.forEach(item => {
+      if (/^\d+$/.test(item)) {
+        let num = parseInt(item);
+        sum += num;
+        if (num % 2 === 0) {
+          even_numbers.push(item);
         } else {
-          special_characters.push(item);
+          odd_numbers.push(item);
         }
-      });
+      } else if (/^[a-zA-Z]+$/.test(item)) {
+        alphabets.push(item.toUpperCase());
+        allAlphabets.push(item);
+      } else {
+        special_characters.push(item);
+      }
+    });
 
-      let combinedAlpha = allAlphabets.join("").split("").reverse();
-      let concat_string = combinedAlpha
-        .map((ch, i) => (i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
-        .join("");
+    let combinedAlpha = allAlphabets.join("").split("").reverse();
+    let concat_string = combinedAlpha
+      .map((ch, i) => (i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
+      .join("");
 
-      return res.status(200).json({
-        is_success: true,
-        user_id: "john_doe_17091999",
-        email: "john@xyz.com",
-        roll_number: "ABCD123",
-        odd_numbers,
-        even_numbers,
-        alphabets,
-        special_characters,
-        sum: sum.toString(),
-        concat_string
-      });
-    } catch (err) {
-      return res.status(500).json({ is_success: false, message: err.message });
-    }
+    return res.status(200).json({
+      is_success: true,
+      user_id: "john_doe_17091999",
+      email: "john@xyz.com",
+      roll_number: "ABCD123",
+      odd_numbers,
+      even_numbers,
+      alphabets,
+      special_characters,
+      sum: sum.toString(),
+      concat_string
+    });
   } else {
     res.status(405).json({ is_success: false, message: "Only POST allowed" });
   }
